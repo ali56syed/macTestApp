38f2fd2e223674378a532ec5ec4931d7d566a0cc
Syed <ali56syed@gmail.com>
2017-12-09 01:47:23 -0600

    Tertiary Check in
    Tertiary Check in

---
+++ macTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace macTestApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("First App on MacOSX");
+        }
+    }
+}
